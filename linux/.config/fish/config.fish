c7b584be4d722ed78267a1b8c692051c7d9b96eb
dydgus5120 <dydgus5120@gmail.com>
2022-11-07 16:13:17 +0900

    Create config.fish

---
+++ linux/.config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+alias vi='nvim'
+alias vim='nvim'
+alias view='nvim -R'
+alias vimdiff='nvim =d'
+
+alias svim='sudo nvim'
